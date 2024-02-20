befaa24b8fd89b4168147eea1edea60b943a26a6
José Antônio <josefreitas788@gmail.com>
2024-02-19 21:17:05 -0300

    delete debianConfigurations

--- debianConfigurations/debianConfigurations.sh
+++
@@ -1,7 +0,0 @@
-sudo apt install -y git curl dirmngr gpg curl gawk
-
-# asdf configs
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
-sed -i -e '$a\. "$HOME/.asdf/asdf.sh"' -e '$a\. "$HOME/.asdf/completions/asdf.bash"' ~/.bashrc
-asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-
